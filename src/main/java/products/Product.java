6f37fe50c0fc1e1dc75d75b6c2962cc8a0715bf9
barokimPL <baroxim@gmail.com>
2023-10-07 19:24:56 +0200

    Add additional constructor that doesn't require price and quantity

--- src/main/java/products/Product.java
+++ src/main/java/products/Product.java
@@ -8,8 +8,8 @@ public class Product {
     private String name;
     private String description;
     private String category;
-    private double price;
-    private int quantityInStock;
+    private Double price;
+    private Integer quantityInStock;
 
     public Product(int id, String name, String description, String category, double price, int quantityInStock) {
         this.id = id;
@@ -19,13 +19,17 @@ public class Product {
         this.price = price;
         this.quantityInStock = quantityInStock;
     }
+    public Product(int id, String name, String description, String category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.price = null;
+        this.quantityInStock = null;
+    }
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
